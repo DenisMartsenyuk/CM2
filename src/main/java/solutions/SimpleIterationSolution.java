9b2976ae7fb2ed37ed9ab9b979185cf516f0805e
DenisMartsenyuk <denmarc@bk.ru>
2021-03-26 01:09:45 +0300

    SimpleIterationSolution implemented

--- src/main/java/solutions/SimpleIterationSolution.java
+++ src/main/java/solutions/SimpleIterationSolution.java
@@ -3,6 +3,16 @@ package solutions;
 import equation.Equation;
 
 public class SimpleIterationSolution implements Solution {
+
+    private double findX0(Equation equation, double a, double b) {
+        for (double i = a; i <= b; i += 0.01) {
+            if (equation.calcValue(i) * equation.calcDerivativeSecondValue(i) > 0) {
+                return i;
+            }
+        }
+        return b;
+    }
+
     @Override
     public String getName() {
         return "Метод простой итерации.";
@@ -11,7 +21,7 @@ public class SimpleIterationSolution implements Solution {
     @Override
     public double findRootEquation(Equation equation, double a, double b, double accuracy) {
         double lambda = -1 / Math.max(equation.calcDerivativeFirstValue(a), equation.calcDerivativeFirstValue(b));
-        double previousX = a;
+        double previousX = findX0(equation, a, b);
         double infelicity = accuracy + 1.0;
         while (infelicity > accuracy) {
             double currentX = previousX + lambda * equation.calcValue(previousX);
