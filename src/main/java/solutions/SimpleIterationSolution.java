4800c5d1774c9fb9657b399e7a422f4facc105d2
DenisMartsenyuk <denmarc@bk.ru>
2021-03-26 00:50:44 +0300

    SimpleIterationSolution implemented

--- src/main/java/solutions/SimpleIterationSolution.java
+++ src/main/java/solutions/SimpleIterationSolution.java
@@ -12,12 +12,13 @@ public class SimpleIterationSolution implements Solution {
     public double findRootEquation(Equation equation, double a, double b, double accuracy) {
         double lambda = -1 / Math.max(equation.calcDerivativeFirstValue(a), equation.calcDerivativeFirstValue(b));
         double previousX = a;
-        double currentX;
-        do {
-            currentX = previousX + lambda * equation.calcValue(previousX);
+        double infelicity = accuracy + 1.0;
+        while (infelicity > accuracy) {
+            double currentX = previousX + lambda * equation.calcValue(previousX);
+            infelicity = Math.abs(currentX - previousX);
             previousX = currentX;
-        } while (Math.abs(currentX - previousX) > accuracy);
-        System.out.println(currentX); //todo вывод
+        }
+        System.out.println(previousX); //todo вывод
         return 0;
     }
 }
